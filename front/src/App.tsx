0df1189cfff4a79003a871591bceac75906dfdae
ValentinGELLY <144439927+ValentinGELLY@users.noreply.github.com>
2025-05-20 18:21:38 +0200

    generate first version pdf

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,11 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import RegisterForm from "@/pages/auth/registerUser";
 import LoginForm from "@/pages/auth/loginUser";
 import ChatPage from "@/pages/chat";
+import IndexPage from "@/pages";
 
 function App() {
   return (
     <Routes>
-      <Route element={<RegisterForm />} path="/register" />
+        <Route element={<RegisterForm />} path="/register" />
+        <Route element={<IndexPage/>} path="/" />
       <Route element={<LoginForm />} path="/login" />
       <Route element={<ChatPage />} path="/chat" />
     </Routes>
